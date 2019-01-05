4dd9c0a6d4790863d7e396d4b103bdc88b65f96a
LAPTOP-U1PBDMRH\aselcukc <aselcukc@gmail.com>
2019-01-05 21:17:08 +0300

    initial

---
+++ src/services/search/routes.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { getPlacesByName } from './SearchController';
+
+export default [
+  {
+    path: '/api/v1/search',
+    method: 'get',
+    handler: async ({ query }: Request, res: Response) => {
+      const result = await getPlacesByName(query.q);
+      res.status(200).send(result);
+    },
+  },
+];
